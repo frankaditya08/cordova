86a36dab36853cf9f4a3a491cc5ce2371944dc5c
frankaditya08 <48473335+frankaditya08@users.noreply.github.com>
2019-05-03 22:19:45 +0700

    Delete db.php

--- www/db.php
+++
@@ -1,4 +0,0 @@
-<?php
- header("Access-Control-Allow-Origin: *");
- $con = mysqli_connect("localhost","id7996950_frankaditya","frankaditya0138","id7996950_tugas7") or die ("could not connect database");
-?>
